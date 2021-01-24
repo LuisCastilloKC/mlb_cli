5e9f9a5146263cb533bdff9dcb4568fdbe1ce20d
Luis Castillo <raul@Luiss-MacBook-Pro.local>
2021-01-24 18:55:18 -0400

    first commit

---
+++ lib/mlb_cli.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require "pry"
+require_relative "mlb_cli/version"
+
+# require all file here
+# require all "TOOLSETS AND LIBRARIES"
+# here is where we require "pry"
+
+module MlbCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
